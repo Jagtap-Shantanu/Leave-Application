703673143cbbb070ee8e86360d185737ae02f43e
Shantanu Jagtap <shantanujagtap93@gmail.com>
2021-10-15 15:33:58 +0530

    leave model set

---
+++ leaves/leave.model.js
@@ -0,0 +1,18 @@
+const Mongoose = require("mongoose")
+const Schema = Mongoose.Schema
+
+const leaveSchema = new Schema ({
+    leaveID: {type: String, unique: true},
+    userID: {type: String},
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    startDate: {type: Date, required: true},
+    endDate: {type: Date, required: true},
+    dayCount: {type: Number},
+    documentUrl: {type: String},
+    status: {type: String}
+})
+
+const leaveModel = Mongoose.model('leaves', leaveSchema)
+
+module.exports = leaveModel
