a8fe13f639d4840ce92334d709f76c8b7efff88f
Shantanu Jagtap <shantanujagtap93@gmail.com>
2021-10-15 14:26:01 +0530

    leave route set

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const userRoute = require("../users")
+const leaveRoute = require("../leaves")
 
 router.use("/user", userRoute)
+
+router.use("/leave", leaveRoute)
 
 module.exports = router
