d57af2d319a6ce1d60cf13caf5cb18188916431f
Shantanu Jagtap <shantanujagtap93@gmail.com>
2021-10-18 22:15:06 +0530

    comments cleanup

--- services/mailNotification.js
+++ services/mailNotification.js
@@ -34,26 +34,5 @@ module.exports = {
     sendNotificationToAdmin
 }
 
-// sendNotificationToAdmin().then((data) => {
-//     for(var i = 0; i< data.length; i++) {
-//         if(!arrayOfUserIDs.includes(data[i].userID)) {
-//             arrayOfUserIDs.push(data[i].userID)
-//         }
-//     }
-//     //console.log("sendNotificationToAdmin ", data)
-//     console.log("sendNotificationToAdmin userIDs", arrayOfUserIDs)
-
-//     for (var j = 0; j< arrayOfUserIDs.length; j++) {
-//         var mailBody = mailer.setNotificationBody(arrayOfUserIDs[j])
-//         mailer.sendMail(mailBody).then(() => {
-//             console.log("Notification mail sent to admin of user")
-//         }).catch((err) => {
-//             console.log("Error while sending notification mail to Admin", err.message)
-//         })
-//     }
-
-// }).catch((err) => {
-//     console.log("sendNotificationToAdmin error", err)
-// })
 
 
