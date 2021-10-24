c8cded85e0e38d2a4db2220af2377501d977817b
Shantanu Jagtap <shantanujagtap93@gmail.com>
2021-10-24 14:48:27 +0530

    deleted middleware file

--- playground.js
+++ playground.js
@@ -41,52 +41,31 @@ const LeaveModel = require("./leaves/leave.model")
 // })
 
 
-const fs = require('fs');
-const excel = require('exceljs');
+// const fs = require('fs');
+// const excel = require('exceljs');
 
-// Create a connection to the MongoDB database
-// MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-//   if (err) throw err;
+// // Create a connection to the MongoDB database
+// // MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+// //   if (err) throw err;
   
-//   let dbo = db.db("gkzdb");
+// //   let dbo = db.db("gkzdb");
   
-    LeaveModel.find({userID: "2c7b3991351"}).then((data) => {
-        console.log(result);
-        let workbook = new excel.Workbook(); //creating workbook
-	    let worksheet = workbook.addWorksheet('LeavesByUserID'); //creating worksheet
+//     LeaveModel.find({userID: "2c7b3991351"}).then((data) => {
+//         console.log(result);
+//         let workbook = new excel.Workbook(); //creating workbook
+// 	    let worksheet = workbook.addWorksheet('LeavesByUserID'); //creating worksheet
 
-        worksheet.addRows(result);
+//         worksheet.addRows(result);
 	
-        // Write to File
-        workbook.xlsx.writeFile("LeavesByUserID.xlsx").then(function() {
-            console.log("file saved!");
-        }).catch((err) => {
-            console.log("Error is ", err)
-        })
+//         // Write to File
+//         workbook.xlsx.writeFile("LeavesByUserID.xlsx").then(function() {
+//             console.log("file saved!");
+//         }).catch((err) => {
+//             console.log("Error is ", err)
+//         })
         
-    }).catch((err) => {
-        consle.log("error is ************", err)
-    })
+//     }).catch((err) => {
+//         consle.log("error is ************", err)
+//     })
     
-	/**
-		[ { _id: 1, name: 'Jack Smith', address: 'Massachusetts', age: 23 },
-		  { _id: 2, name: 'Adam Johnson', address: 'New York', age: 27 },
-		  { _id: 3, name: 'Katherin Carter', address: 'Washington DC', age: 26 },
-		  { _id: 4, name: 'Jack London', address: 'Nevada', age: 33 },
-		  { _id: 5, name: 'Jason Bourne', address: 'California', age: 36 } ]	
-	*/
-	
-	
-	
-	//  WorkSheet Header
-	// worksheet.columns = [
-	// 	{ header: 'Id', key: '_id', width: 10 },
-	// 	{ header: 'Name', key: 'name', width: 30 },
-	// 	{ header: 'Address', key: 'address', width: 30},
-	// 	{ header: 'Age', key: 'age', width: 10, outlineLevel: 1}
-	// ];
-	
-	// Add Array Rows
-	
-
 
